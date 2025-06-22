45fa770bb49e76e564be1d70864b3b7912358de9
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-06-22 15:54:45 +0300

    add database configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import {connectDB} from "./lib/db.js"
 
-const port = 5001
-
+dotenv.config()
+const port = process.env.port
 const app = express()
 
 app.use('/api/auth', authRoutes)
 
 app.listen(port,()=>{
     console.log(`server started listening on port ${port}!`)
+    connectDB()
 })
