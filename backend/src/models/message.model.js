d7da5dd96533ce9777e406f87fd0ee2f7da39bd7
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-07 21:15:56 +0300

    add message model

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const messageSchema = mongoose.Schema({
+    senderId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    recieverId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    text: {
+        type: String
+    },
+
+    image: {
+        type: String
+    },
+},
+    { timestamps: true }
+);
+
+const Message = mongoose.model("Message",messageSchema);
+
+export default Message;
