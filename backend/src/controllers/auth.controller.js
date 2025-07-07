4e93c68b1f4c5ec5f0ac58f968dd33381aa900dc
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-07 18:16:46 +0300

    add the login and logout endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import { genearteToken } from "../lib/utils.js"
 export const signup = async (req, res) => {
     const { email, fullName, password } = req.body
     try {
-        if(!email || !password || !fullName)
+        if (!email || !password || !fullName)
             return res.status(400).json({ message: "All Fields are required" })
         if (password.length < 6)
             return res.status(400).json({ message: "Password must be at least 6 characters" })
@@ -37,14 +37,40 @@ export const signup = async (req, res) => {
         }
     } catch (error) {
         console.log("error in signup controller", error.message);
-        res.status(500).json({message: "Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
     }
 }
 
-export const login = (req, res) => {
-    res.send("login")
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = User.findOne({ email })
+        if (!user)
+            return res.status(400).json({ message: "Invalid Credentials!" });
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (isPasswordCorrect)
+            return res.status(400).json({ message: "Invalid Credentials!" });
+        genearteToken(email, req);
+        res.status(201).json(
+            {
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            }
+        )
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+        res.status(500).json({ message: "Internal server error" })
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logout")
+    try {
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message: "Logged out successfully!"})
+    } catch (error) {
+        console.log("Error in logout controller", error.message)
+        res.status(500).json({ message: "Internal server error" })
+    }
 }
