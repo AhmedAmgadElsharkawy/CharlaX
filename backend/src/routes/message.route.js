6d923382f05eff780426efd04d1c9a80b274e121
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-07 22:53:40 +0300

    fix message route error

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import protectRoute from "../lib/utils.js"
+import {protectRoute} from "../middleware/auth.middleware.js"
 import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js"
 
 const router = express.Router();
