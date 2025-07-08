4469a2be28941b2f03b75619101b21664571d964
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-08 18:53:41 +0300

    add the toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,8 @@ import { useAuthStore } from './store/useAuthStore'
 
 import { Loader } from 'lucide-react';
 
+import { Toaster } from 'react-hot-toast'
+
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
@@ -23,26 +25,28 @@ function App() {
   }, [checkAuth])
 
 
-  if (isCheckingAuth && !authUser){
-    return(
+  if (isCheckingAuth && !authUser) {
+    return (
       <div className='flex items-center justify-center h-screen'>
         <Loader className='size-10 animate-spin' />
       </div>
     )
   }
 
-    return (
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}></Route>
-          <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />}></Route>
-          <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />}></Route>
-          <Route path='/settings' element={<SettingsPage /> }></Route>
-          <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />}></Route>
-        </Routes>
-      </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}></Route>
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />}></Route>
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />}></Route>
+        <Route path='/settings' element={<SettingsPage />}></Route>
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />}></Route>
+      </Routes>
+
+      <Toaster/>
+    </div>
+  )
 }
 
 export default App
