5e432a684371ab14bce76a8db9dd4025459fe892
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-12 18:43:12 +0300

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,14 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
+  // strict mode checks are run in development mode only
+  // strict mode checks are run twice in development mode to help identify side effects so use effect run twice
+  // strict mode does not affect production builds
   <StrictMode>
     <Router>
       <App />
     </Router>
   </StrictMode>,
 )
+
+
