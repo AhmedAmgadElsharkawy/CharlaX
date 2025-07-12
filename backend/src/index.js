9f2aa008146afa9d4b0e1afa5952bb26c80bdb56
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-12 18:44:08 +0300

    make socket.io layer

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,26 +2,29 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
-import {connectDB} from "./lib/db.js"
+import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import {server, app} from "./lib/socket.js"
 
 dotenv.config()
 const port = process.env.port
-const app = express()
 
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ limit: "5mb", extended: true }))
 app.use(cookieParser())
+
+// This ensures that the frontend (on a different origin like 5173) can call your Express routes.
 app.use(cors({
     origin: "http://localhost:5173",
-    credentials:true
+    credentials: true
+    // It tells the CORS middleware (Cross-Origin Resource Sharing) to allow the client to send cookies, authorization headers, or TLS certificates
 }))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
-app.listen(port,()=>{
+server.listen(port, () => {
     console.log(`server started listening on port ${port}!`)
     connectDB()
 })
