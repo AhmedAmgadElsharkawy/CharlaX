d32ec7d92581fd9a23c1f938dd4b828df1685a5e
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-07-12 20:17:56 +0300

    update base url

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,14 +5,19 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
-import {server, app} from "./lib/socket.js"
+import { server, app } from "./lib/socket.js"
+// path is a built-in Node.js module.
+import path from "path"
 
 dotenv.config()
 const port = process.env.port
+// get the absolute path of the current directory
+const __dirname = path.resolve() 
 
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ limit: "5mb", extended: true }))
 app.use(cookieParser())
+
 
 // This ensures that the frontend (on a different origin like 5173) can call your Express routes.
 app.use(cors({
@@ -23,6 +28,15 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    // Serve static files from the React frontend app (for the hosting server to the frontend)
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        // This line is telling your Express server to send the built frontend page (index.html) to the client when a request doesn't match any backend route
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(port, () => {
     console.log(`server started listening on port ${port}!`)
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
-    withCredentials: true
-})
+    // When you use Vite (the build tool you’re using here), it injects a special object called import.meta.env into your code
+    // While developing (vite / npm run dev):
+    // import.meta.env.MODE is "development", so baseURL becomes
+    // http://localhost:5001/api — your local backend.
+
+    // When you run npm run build (production bundle):
+    // The mode defaults to "production", so baseURL becomes just "/api".
+    // That assumes your frontend and backend are served from the same origin in prod.
+
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    withCredentials: true,
+});
