f4b5e14907e4e246503ba6926d1adbe379df57ab
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-09-06 23:49:18 +0300

    fix the infinite refresh requests

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,18 +1,26 @@
 import axios from "axios";
+import {useAuthStore} from "../store/useAuthStore"
 
-const axiosInstance = axios.create({
-    // When you use Vite (the build tool you’re using here), it injects a special object called import.meta.env into your code
-    // While developing (vite / npm run dev):
-    // import.meta.env.MODE is "development", so baseURL becomes
-    // http://localhost:5001/api — your local backend.
+// When you use Vite (the build tool you’re using here), it injects a special object called import.meta.env into your code
+// While developing (vite / npm run dev):
+// import.meta.env.MODE is "development", so baseURL becomes
+// http://localhost:5001/api — your local backend.
 
-    // When you run npm run build (production bundle):
-    // The mode defaults to "production", so baseURL becomes just "/api".
-    // That assumes your frontend and backend are served from the same origin in prod.
-
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+// When you run npm run build (production bundle):
+// The mode defaults to "production", so baseURL becomes just "/api".
+// That assumes your frontend and backend are served from the same origin in prod.
+const baseConfig = {
+    baseURL:
+        import.meta.env.MODE === "development"
+            ? "http://localhost:5001/api"
+            : "/api",
     withCredentials: true,
-});
+};
+
+const axiosInstance = axios.create(baseConfig);
+
+// refreshAxios has the same config but no interceptors
+const refreshAxios = axios.create(baseConfig);
 
 
 axiosInstance.interceptors.response.use(
@@ -20,15 +28,16 @@ axiosInstance.interceptors.response.use(
     async (error) => {
         if (error.response?.status == 401) {
             try {
-                await axiosInstance.get("/auth/refresh")
+                await refreshAxios.get("/auth/refresh")
                 console.log("refresh")
                 return axiosInstance(error.config);
             } catch (refreshError) {
                 console.error("Refresh failed", refreshError);
+                useAuthStore.getState().logout()
             }
         }
         return Promise.reject(error)
     }
 )
 
-export {axiosInstance}
+export { axiosInstance }
